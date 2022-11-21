a2bf632b81be05b5b58e397625f9503a8158aa89
EC2 Default User <ec2-user@ip-172-31-13-179.ap-northeast-1.compute.internal>
2022-11-21 14:17:38 +0000

    [Practice]変数var,let,constについて

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -2,13 +2,13 @@ let hello = 'Hello World';
 alert(hello);
 
 // 割り算
-alert(10 / 5);
+// alert(10 / 5);
 
-alert("Hello" + "World");
+// alert("Hello" + "World");
 
 let str1 = "Hello";
 let str2 = "World!!";
-alert(str1 + str2);
+// alert(str1 + str2);
 
 let orange = 100;
 let apple = 120;
@@ -29,7 +29,7 @@ while(num < max){
   count = count + 1;
 }
 
-alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
 
 let i;
 let num2 = 0;
@@ -38,4 +38,13 @@ for(i = 1; i < 11; i++){
   num2 = num2 + i;
 }
 
-alert('1から10まで足し算した結果は' + num2 + 'です');
+// alert('1から10まで足し算した結果は' + num2 + 'です');
+
+var str = "webcamp"
+
+function foo(){
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+foo()
